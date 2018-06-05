dcbe5c8285de96171d939be5bdc2919be34bd6ce
Nuno <nuno.ferreira@gamesinc.co.uk>
2018-06-05 13:44:06 +0100

    added double dice generation

---
+++ src/main.elm
@@ -0,0 +1,80 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, h1, text, button)
+import Html.Events exposing (onClick)
+import Random exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { die1Face : Int
+    , die2Face : Int
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Roll
+    | NewFace ( Int, Int )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Roll ->
+            ( model, Random.generate NewFace doubleDieGenerator )
+
+        NewFace ( newFace1, newFace2 ) ->
+            ( { die1Face = newFace1, die2Face = newFace2 }, Cmd.none )
+
+
+dieGenerator : Random.Generator Int
+dieGenerator =
+    Random.int 1 6
+
+
+doubleDieGenerator : Random.Generator ( Int, Int )
+doubleDieGenerator =
+    Random.pair dieGenerator dieGenerator
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text (toString model.die1Face) ]
+        , h1 [] [ text (toString model.die2Face) ]
+        , button [ onClick Roll ] [ text "Roll" ]
+        ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- INIT
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { die1Face = 1, die2Face = 1 }, Cmd.none )
